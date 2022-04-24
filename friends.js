092f1a5645d7c5321bf267d914e6690567dcfb71
yustinbieber <98377062+yustinbieber@users.noreply.github.com>
2022-04-24 20:06:59 -0300

    Update friends.js

--- friends.js
+++ friends.js
@@ -18,3 +18,24 @@ fetch(`${"https://tokyo.tokyocity.repl.co/avatar/684902555670806546"}`, {
 });
 
 fetch("https://tokyo.tokyocity.repl.co/user/684902555670806546").then((res) => res.json()).then((json) => { Object.keys(json).forEach((key) => { const elem = document.getElementsByClassName("jeje"); elem[0].textContent = json[key]; }); })
+
+//friend2
+
+'use strict';
+fetch(`${"https://tokyo.tokyocity.repl.co/avatar/864714779833401374"}`, {
+  method : "GET",
+  mode : "cors"
+}).then(function(responseJson) {
+  if (responseJson.ok) {
+    return responseJson.json();
+  }
+  return Promise.reject(responseJson);
+}).then(function(fieldDescribe) {
+  console.log(fieldDescribe.url);
+  document.getElementById("valu").src = fieldDescribe.url;
+}).catch(function($ownerNode) {
+  console.warn("Something went wrong.", $ownerNode);
+  document.getElementById("valu");
+});
+
+fetch("https://tokyo.tokyocity.repl.co/user/864714779833401374").then((res) => res.json()).then((json) => { Object.keys(json).forEach((key) => { const elem = document.getElementsByClassName("valu"); elem[0].textContent = json[key]; }); })
